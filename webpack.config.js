07cf773efa29a297234a4c683af2aa92e48c7ea3
Rina Asimo <rvolenc@gmail.com>
2024-04-30 13:53:33 +0400

    feat: add webpack configuration file

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const isDevelopment = mode === 'development';
+  const devtool = isDevelopment ? 'source-map' : undefined;
+  return {
+    mode,
+    devtool,
+    entry: './src/index.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: [path.resolve(__dirname, 'src')],
+          use: 'ts-loader',
+        },
+        {
+          test: /\.(jpg|png|svg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        favicon: path.resolve(__dirname, 'src/assets/favicon.ico'),
+        filename: 'index.html',
+      }),
+      // new EslintPlugin({ extensions: ['ts'] }), // запускает проверку линтера при сборке
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      assetModuleFilename: 'assets/[hash][ext]',
+    },
+    devServer: {
+      static: path.join(__dirname, 'src'),
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+  };
+};
