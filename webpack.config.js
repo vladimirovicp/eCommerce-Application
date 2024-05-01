1217a7bdcf84a77de613a963aa5a1484b39adb60
asimo-git <113469479+asimo-git@users.noreply.github.com>
2024-05-01 13:45:40 +0400

    Ecomm 1_08-1_12: Development Environment Configuration (#3)
    * feat: add package.json, install husky
    
    * feat: add webpack configuration file
    
    * feat: add ts and jest configuration file
    
    * feat: add eslint and prettier configuration file, add husky hook
    
    * fix: change husky configuration
    
    * fix: change webpack configure
    
    * feat: add sass support in webpack.config.js
    
    * feat: update package.json
    
    * feat: change ts configuration
    
    * feat: add favicon

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const EslintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development';
+  const isDevelopment = mode === 'development';
+  const devtool = isDevelopment ? 'source-map' : undefined;
+  return {
+    mode,
+    devtool,
+    entry: './src/app.ts',
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          include: [path.resolve(__dirname, 'src')],
+          use: 'ts-loader',
+        },
+        {
+          test: /\.(jpg|png|svg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        favicon: path.resolve(__dirname, 'src/assets/img/favicon.ico'),
+        filename: 'index.html',
+      }),
+      new EslintPlugin({ extensions: ['ts'] }),
+    ],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      assetModuleFilename: 'assets/[hash][ext]',
+    },
+    devServer: {
+      static: path.join(__dirname, 'src'),
+      compress: true,
+      port: 9000,
+      open: true,
+    },
+  };
+};
