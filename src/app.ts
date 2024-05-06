6e736a0ad4ab7ea6fcc3d41e950a181f75df2f45
Rina Asimo <rvolenc@gmail.com>
2024-05-06 19:09:44 +0400

    feat: implement footer

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
+import FooterView from './app/components/footer/footer';
 import './assets/scss/_global.scss';
+
+class App {
+  constructor() {
+    this.createView();
+  }
+
+  private createView(): void {
+    // const headerView = new HeaderView();
+    // const mainView = new MainView();
+    const footer = new FooterView().getHtmlElement();
+
+    document.body.appendChild(footer);
+  }
+}
+
+// eslint-disable-next-line
+const app = new App();
