96487fee9a75f8467aa175d4bd47f7c28743a3c7
Svetlana Vorokhobina <svetlana.vorokhobina@gmail.com>
2024-05-07 23:05:42 +1200

    feat: add templates for input error messages

--- src/app/temp-error-messages/error-login.ts
+++
@@ -1,7 +0,0 @@
-export const enum EmailErrors {
-  Required = 'Field is required',
-  Incorrect = 'Invalid email address',
-  Hint = `Email address must match the pattern 'username@domain.extension'.
-  Username and domain name should have at least 2 symbols, be separated with '@', and include only lowercase letters, digits or symbols '. _ #'`,
-  Whitespace = 'Whitespaces are not allowed',
-}
