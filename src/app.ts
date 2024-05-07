e5522d87a5ffac163298c9232fa18b32e518c445
asimo-git <113469479+asimo-git@users.noreply.github.com>
2024-05-07 13:05:31 +0400

    feat: implement footer (#11)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
+import FooterView from './app/components/footer/footer';
 import './assets/scss/_global.scss';
 import NotFound from './app/pages/404/NotFound';
+
+class App {
+  constructor() {
+    this.createView();
+  }
+
+  private createView(): void {
+    // const headerView = new HeaderView();
+    // const mainView = new MainView();
+    const footer = new FooterView().getHtmlElement();
+
+    document.body.appendChild(footer);
+  }
+}
+
+// eslint-disable-next-line
+const app = new App();
 
 const notFound = new NotFound();
 notFound.render();
