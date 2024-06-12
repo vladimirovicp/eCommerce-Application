31b1c8f80df7955c81aaddac9f4c68c8af4753b2
asimo-git <113469479+asimo-git@users.noreply.github.com>
2024-06-12 12:49:26 +0400

    Сart page functionality (#61)
    * feat: implement removing product in cart page
    
    * feat: implement remove all button
    
    * feat: add changing quantity of product in cart
    
    * feat: implement use of promocodes
    
    * feat: add error handling when entering promocode
    
    * feat: add empty cart message

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -1,11 +1,13 @@
-import { Cart, ProductProjection } from '@commercetools/platform-sdk';
+import {
+  // ByProjectKeyRequestBuilder,
+  Cart,
+  CartUpdateAction,
+  ClientResponse,
+  DiscountCodePagedQueryResponse,
+  ProductProjection,
+} from '@commercetools/platform-sdk';
+import modalWindowCreator from '../components/modal-window';
 import { apiRoot, apiRoots } from './build-client';
-
-// interface Cart {
-//   id: string;
-//   version: number;
-//   products: LineItem[];
-// }
 
 export async function updateProducts(
   limit: number,
@@ -48,13 +50,11 @@ export async function updateProducts(
 }
 
 export async function getTheCart(): Promise<Cart | undefined> {
-  // let cart: Cart;
   const currentApiRoot = apiRoots.byRefreshToken ? apiRoots.byRefreshToken : apiRoots.byAnonymousId;
   // получить корзину
   if (currentApiRoot) {
     try {
       const response = await currentApiRoot.me().activeCart().get().execute();
-      // cart = { id: response.body.id, version: response.body.version, products: response.body.lineItems };
       return response.body;
     } catch (error) {
       if (error instanceof Error && 'code' in error && error.code === 404) {
@@ -69,13 +69,173 @@ export async function getTheCart(): Promise<Cart | undefined> {
               },
             })
             .execute();
-          // cart = { id: response.body.id, version: response.body.version, products: response.body.lineItems };
           return response.body;
         } catch (createError) {
           console.error('Error creating new cart:', createError);
         }
       } else {
         console.error('Error retrieving active cart:', error);
+      }
+    }
+  }
+  return undefined;
+}
+
+export async function removeLineFromCart(cart: Cart, productId: string): Promise<ClientResponse<Cart> | undefined> {
+  const currentApiRoot = apiRoots.byRefreshToken ? apiRoots.byRefreshToken : apiRoots.byAnonymousId;
+
+  if (currentApiRoot) {
+    try {
+      // находим в корзине нужную строчку с искомым продуктом
+      const lineItem = cart.lineItems.find((item) => item.productId === productId);
+      const response = await currentApiRoot
+        .carts()
+        .withId({ ID: cart.id })
+        .post({
+          body: {
+            version: cart.version,
+            actions: [
+              {
+                action: 'removeLineItem',
+                lineItemId: lineItem?.id,
+              },
+            ],
+          },
+        })
+        .execute();
+      return response;
+    } catch (error) {
+      console.error('Error removing product from cart:', error);
+      return undefined;
+    }
+  }
+  return undefined;
+}
+
+export async function clearCart(cart: Cart): Promise<ClientResponse<Cart> | undefined> {
+  // добавить лоадер?
+  const currentApiRoot = apiRoots.byRefreshToken ? apiRoots.byRefreshToken : apiRoots.byAnonymousId;
+
+  if (currentApiRoot) {
+    try {
+      // формируем массив товаров, чтобы удаление всех строк было в одном обращени к серверу
+      const actions: CartUpdateAction[] = cart.lineItems.map((item) => ({
+        action: 'removeLineItem',
+        lineItemId: item.id,
+      }));
+
+      const response = await currentApiRoot
+        .carts()
+        .withId({ ID: cart.id })
+        .post({
+          body: {
+            version: cart.version,
+            actions,
+          },
+        })
+        .execute();
+
+      return response;
+    } catch (error) {
+      console.error('Error removing products from cart:', error);
+      return undefined;
+    }
+  }
+  return undefined;
+}
+
+export async function updateProductQuantity(
+  cart: Cart,
+  productId: string,
+  increment: boolean
+): Promise<ClientResponse<Cart> | undefined> {
+  const currentApiRoot = apiRoots.byRefreshToken ? apiRoots.byRefreshToken : apiRoots.byAnonymousId;
+
+  if (currentApiRoot) {
+    try {
+      const lineItem = cart.lineItems.find((item) => item.productId === productId);
+      if (lineItem) {
+        const newQuantity = increment ? lineItem.quantity + 1 : lineItem.quantity - 1;
+        if (newQuantity > 0) {
+          const response = await currentApiRoot
+            .carts()
+            .withId({ ID: cart.id })
+            .post({
+              body: {
+                version: cart.version,
+                actions: [
+                  {
+                    action: 'changeLineItemQuantity',
+                    lineItemId: lineItem.id,
+                    quantity: newQuantity,
+                  },
+                ],
+              },
+            })
+            .execute();
+          return response;
+        }
+      }
+      return undefined;
+    } catch (error) {
+      console.error('Error updating product quantity in cart:', error);
+      return undefined;
+    }
+  }
+  return undefined;
+}
+
+async function checkIsPromoCodeApplied(cart: Cart, promoCode: string): Promise<boolean> {
+  const currentApiRoot = apiRoots.byRefreshToken ? apiRoots.byRefreshToken : apiRoots.byAnonymousId;
+  if (currentApiRoot) {
+    const codeResponse: ClientResponse<DiscountCodePagedQueryResponse> = await currentApiRoot
+      .discountCodes()
+      .get({
+        queryArgs: {
+          where: `code="${promoCode}"`,
+        },
+      })
+      .execute();
+    if (codeResponse.body.results.length > 0) {
+      const codeId = codeResponse.body.results[0].id;
+      const codeInCart = cart.discountCodes.find((item) => item.discountCode.id === codeId);
+      if (codeInCart && codeInCart.state === 'MatchesCart') {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export async function applyPromoCode(cart: Cart, promoCode: string): Promise<ClientResponse<Cart> | undefined> {
+  const currentApiRoot = apiRoots.byRefreshToken ? apiRoots.byRefreshToken : apiRoots.byAnonymousId;
+
+  if (currentApiRoot) {
+    try {
+      if (await checkIsPromoCodeApplied(cart, promoCode)) {
+        modalWindowCreator.showModalWindow('error', `The discount code '${promoCode}' has already been applied`);
+        return undefined;
+      }
+      const response = await currentApiRoot
+        .carts()
+        .withId({ ID: cart.id })
+        .post({
+          body: {
+            version: cart.version,
+            actions: [
+              {
+                action: 'addDiscountCode',
+                code: promoCode,
+              },
+            ],
+          },
+        })
+        .execute();
+      return response;
+    } catch (error) {
+      if (error instanceof Error && 'code' in error && error.code === 400) {
+        modalWindowCreator.showModalWindow('error', `The discount code '${promoCode}' was not found`);
+        return undefined;
       }
     }
   }
--- src/app/pages/cart/index.ts
+++ src/app/pages/cart/index.ts
@@ -1,7 +1,8 @@
 import { Cart, LineItem } from '@commercetools/platform-sdk';
 import '../../../assets/scss/page/basket.scss';
-import { getTheCart } from '../../api/products';
+import { applyPromoCode, clearCart, getTheCart, removeLineFromCart, updateProductQuantity } from '../../api/products';
 import View from '../../common/view';
+import modalWindowCreator from '../../components/modal-window';
 import { Pages } from '../../router/pages';
 import Router from '../../router/router';
 import ElementCreator from '../../util/element-creator';
@@ -13,6 +14,12 @@ export default class CartPage extends View {
 
   private cart: Cart | undefined = undefined;
 
+  private totalPriceElement: ElementCreator | undefined = undefined;
+
+  private oldPriceElement: ElementCreator | undefined = undefined;
+
+  private listContainer: ElementCreator<HTMLDivElement>;
+
   constructor(router: Router) {
     const params = {
       tag: 'div',
@@ -20,28 +27,45 @@ export default class CartPage extends View {
     };
     super(params);
     this.router = router;
+    this.listContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__cards'] });
     this.setContent();
   }
 
   private async setContent(): Promise<void> {
     this.cart = await getTheCart();
 
-    const basketMain = new ElementCreator({ classNames: ['basket__content'] });
-    basketMain.addInnerElements([this.createRemoveButton(), this.createItemList()]);
+    if (this.cart?.lineItems.length === 0) {
+      this.createEmptyCartPage();
+    } else {
+      const basketMain = new ElementCreator({ classNames: ['basket__content'] });
+      basketMain.addInnerElements([this.createRemoveAllButton(), this.createItemList()]);
 
-    const basketAside = new ElementCreator({ classNames: ['basket__aside'] });
-    basketAside.addInnerElements([this.createPromocode(), this.createTotalPrice()]);
+      const basketAside = new ElementCreator({ classNames: ['basket__aside'] });
+      basketAside.addInnerElements([this.createPromocode(), this.createTotalPrice()]);
 
-    this.viewElementCreator.addInnerElements([basketMain, basketAside]);
+      this.viewElementCreator.addInnerElements([basketMain, basketAside]);
+    }
   }
 
-  private createRemoveButton(): ElementCreator<HTMLDivElement> {
+  private createRemoveAllButton(): ElementCreator<HTMLDivElement> {
     const buttonContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__wrapper-btn'] });
     const removeAllButton = new ElementCreator<HTMLButtonElement>({
       tag: 'button',
       classNames: ['btn-default'],
       textContent: 'Remove all',
-      callback: (): void => {},
+      callback: async (): Promise<void> => {
+        if (this.cart && this.cart.lineItems.length > 0) {
+          const response = await clearCart(this.cart);
+          if (response?.statusCode === 200) {
+            this.cart = response.body;
+            this.listContainer.getElement().innerHTML = '';
+            // TODO добавить "ваша корзина пуста" и ссылку на каталог
+            if (this.totalPriceElement) this.totalPriceElement.getElement().textContent = '$ 0';
+          } else {
+            modalWindowCreator.showModalWindow('error', 'Failed to remove products from cart. Please try again');
+          }
+        }
+      },
     });
 
     buttonContainer.addInnerElements([removeAllButton]);
@@ -49,32 +73,43 @@ export default class CartPage extends View {
   }
 
   private createItemList(): ElementCreator<HTMLDivElement> {
-    const listContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__cards'] });
-
-    if (this.cart && this.cart.lineItems.length > 0) {
+    if (this.cart) {
       this.cart.lineItems.forEach((item) => {
         const card = this.createBasketCard(item);
-        listContainer.addInnerElements([card]);
+        this.listContainer.addInnerElements([card]);
       });
-    } else {
-      // TODO добавить "ваша корзина пуста" и ссылку на каталог
     }
-    return listContainer;
+    return this.listContainer;
+  }
+
+  private createEmptyCartPage(): void {
+    const message = new ElementCreator<HTMLDivElement>({
+      textContent: 'Your cart is still empty. Would you like to continue shopping?',
+    });
+    const catalogButton = new ElementCreator<HTMLDivElement>({
+      tag: 'button',
+      classNames: ['btn-default'],
+      textContent: 'Catalog',
+      callback: (): void => {
+        this.router.navigate(`${Pages.CATALOG}`);
+      },
+    });
+    this.viewElementCreator.getElement().innerHTML = '';
+    this.viewElementCreator.addInnerElements([message, catalogButton]);
   }
 
   private createBasketCard(item: LineItem): ElementCreator<HTMLDivElement> {
     const cardContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card'] });
-
     const {
       productId,
       name,
       variant: { images, prices },
       quantity,
     } = item;
-
     const imgContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card-img'] });
     imgContainer.getElement().innerHTML = `<img class="img-full" src="${images?.[0]?.url || ''}" alt="${name['en-GB']}">`;
 
+    const cardContant = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card-content'] });
     const nameContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card-name'] });
     const nameElement = new LinkCreator({
       textContent: name['en-GB'],
@@ -84,37 +119,44 @@ export default class CartPage extends View {
     });
     nameContainer.addInnerElements([nameElement]);
 
+    const cardControl = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card-control'] });
     const deleteButton = new ElementCreator<HTMLDivElement>({
       classNames: ['basket__card-delete'],
-      callback: (): void => {},
+      callback: async (): Promise<void> => {
+        this.removeProduct(productId, cardContainer);
+      },
     });
-
-    const counterContainer = this.createItemCounter(quantity);
+    cardControl.addInnerElements([deleteButton, this.createItemCounter(productId, quantity)]);
 
     const priceContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card-price'] });
     const price = prices?.[0]?.discounted?.value.centAmount || 0;
     priceContainer.getElement().innerHTML = `<div class="price__current">$ ${price / 100}</div>`;
 
-    cardContainer.addInnerElements([imgContainer, nameContainer, deleteButton, counterContainer, priceContainer]);
+    cardContant.addInnerElements([nameContainer, cardControl, priceContainer]);
+    cardContainer.addInnerElements([imgContainer, cardContant]);
     return cardContainer;
   }
 
-  private createItemCounter(quantity: number): ElementCreator<HTMLDivElement> {
+  private createItemCounter(productId: string, quantity: number): ElementCreator<HTMLDivElement> {
     const counterContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__card-number'] });
 
-    const minusElement = new ElementCreator<HTMLDivElement>({
-      classNames: ['number__minus'],
-      textContent: '-',
-      callback: (): void => {},
-    });
     const valueElement = new ElementCreator<HTMLDivElement>({
       classNames: ['number__value'],
       textContent: `${quantity}`,
     });
+    const minusElement = new ElementCreator<HTMLDivElement>({
+      classNames: ['number__minus'],
+      textContent: '-',
+      callback: (): void => {
+        this.updateQuantity(productId, valueElement.getElement(), false);
+      },
+    });
     const plusElement = new ElementCreator<HTMLDivElement>({
       classNames: ['number__plus'],
       textContent: '+',
-      callback: (): void => {},
+      callback: (): void => {
+        this.updateQuantity(productId, valueElement.getElement(), true);
+      },
     });
 
     counterContainer.addInnerElements([minusElement, valueElement, plusElement]);
@@ -137,7 +179,20 @@ export default class CartPage extends View {
       tag: 'button',
       classNames: ['btn-default'],
       textContent: 'Apply',
-      callback: (): void => {},
+      callback: async (): Promise<void> => {
+        const promoCode = promoInput.getElement().value;
+        if (!promoCode) {
+          modalWindowCreator.showModalWindow('error', 'Promo code cannot be empty!');
+          return;
+        }
+        if (this.cart) {
+          const response = await applyPromoCode(this.cart, promoCode);
+          if (response) {
+            this.cart = response.body;
+            this.updateTotalCosts();
+          }
+        }
+      },
     });
     buttonContainer.addInnerElements([applyButton]);
 
@@ -148,24 +203,84 @@ export default class CartPage extends View {
   private createTotalPrice(): ElementCreator<HTMLDivElement> {
     const priceContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__total'] });
 
-    const totalPrice = this.cart ? this.cart.totalPrice.centAmount : 0;
-
     const infoContainer = new ElementCreator<HTMLDivElement>({ classNames: ['basket__total-info'] });
+
+    this.oldPriceElement = new ElementCreator<HTMLDivElement>({
+      classNames: ['basket__total-price', 'price__old'],
+    });
+
     const priceText = new ElementCreator<HTMLDivElement>({
       classNames: ['basket__total-text'],
       textContent: 'Total cost',
     });
-    const priceValue = new ElementCreator<HTMLDivElement>({
+    this.totalPriceElement = new ElementCreator<HTMLDivElement>({
       classNames: ['basket__total-price'],
-      textContent: `$ ${totalPrice / 100}`,
     });
-    infoContainer.addInnerElements([priceText, priceValue]);
+
+    this.updateTotalCosts();
+    infoContainer.addInnerElements([priceText, this.totalPriceElement]);
 
     const buttonContainer = new ElementCreator<HTMLDivElement>({ classNames: ['form__button'] });
     // кнопка без колбэка, просто заглушка
     buttonContainer.getElement().innerHTML = `<button class="btn-default">Place an order</button>`;
 
-    priceContainer.addInnerElements([infoContainer, buttonContainer]);
+    priceContainer.addInnerElements([this.oldPriceElement, infoContainer, buttonContainer]);
     return priceContainer;
+  }
+
+  private async removeProduct(productId: string, card: ElementCreator): Promise<void> {
+    if (this.cart) {
+      const response = await removeLineFromCart(this.cart, productId);
+      if (response?.statusCode === 200) {
+        card.getElement()?.remove();
+        this.cart = response.body;
+        this.updateTotalCosts();
+        if (this.cart.lineItems.length === 0) {
+          this.createEmptyCartPage();
+        }
+      } else {
+        modalWindowCreator.showModalWindow('error', 'Failed to remove product from cart. Please try again');
+      }
+    }
+  }
+
+  private async updateQuantity(productId: string, counter: HTMLDivElement, isPlusOne: boolean): Promise<void> {
+    if (this.cart) {
+      const currentCounter = counter;
+      const response = await updateProductQuantity(this.cart, productId, isPlusOne);
+      if (response?.statusCode === 200) {
+        this.cart = response.body;
+        const lineItem = this.cart.lineItems.find((item) => item.productId === productId);
+        currentCounter.textContent = String(lineItem?.quantity);
+        this.updateTotalCosts();
+      }
+    }
+  }
+
+  private updateTotalCosts(): void {
+    if (this.cart) {
+      if (
+        this.cart.discountCodes.length > 0 &&
+        this.cart.discountCodes.find((item) => item.state === 'MatchesCart') &&
+        this.oldPriceElement
+      ) {
+        let originalPrice: number = 0;
+        this.cart.lineItems.forEach((item) => {
+          originalPrice += item.price.discounted
+            ? item.price.discounted.value.centAmount * item.quantity
+            : item.price.value.centAmount * item.quantity;
+        });
+        if (originalPrice !== this.cart.totalPrice.centAmount) {
+          this.oldPriceElement.getElement().textContent = `$ ${originalPrice / 100}`;
+        } else {
+          this.oldPriceElement.getElement().textContent = ``;
+        }
+      }
+
+      if (this.totalPriceElement) {
+        this.totalPriceElement.getElement().textContent = `$ ${this.cart.totalPrice.centAmount / 100}`;
+        console.log(this.totalPriceElement.getElement().textContent);
+      }
+    }
   }
 }
